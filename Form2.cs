00df90ca09f7d6908d1ea0aa1079742f7a5215cf
Timprogramiranje <83233366+Timprogramiranje@users.noreply.github.com>
2021-04-27 05:18:10 +0200

    Add files via upload

---
+++ Form2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BIke___app
+{
+    public partial class FormTutzapoprbic : Form
+    {
+        public FormTutzapoprbic()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormTutzapoprbic_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormTutzapoprbic_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
